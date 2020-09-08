afbf3527a0b77f612a6ba528ba206fc3e508d9c7
Andreinecul98 <Andnec98!>
2020-09-08 12:23:16 +0300

    Reupdate

--- src/main/java/com/ApiTheBest/AppointmentAPI/MedicalAppointment.java
+++ src/main/java/com/ApiTheBest/AppointmentAPI/MedicalAppointment.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiMicSiTareApplication {
+public class MedicalAppointment {
 
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiMicSiTareApplication.class, args);
+		SpringApplication.run(MedicalAppointment.class, args);
 	}
 
 }
