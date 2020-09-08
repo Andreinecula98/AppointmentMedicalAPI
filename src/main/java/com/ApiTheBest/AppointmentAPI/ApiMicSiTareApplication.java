07788eb17bb915ec15743074deac87ff8f36161a
Andreinecul98 <Andnec98!>
2020-09-08 12:18:47 +0300

    Upload Project

---
+++ src/main/java/com/ApiTheBest/AppointmentAPI/ApiMicSiTareApplication.java
@@ -0,0 +1,15 @@
+package com.ApiTheBest.AppointmentAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMicSiTareApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMicSiTareApplication.class, args);
+	}
+
+}
